e577fb991ff37c1c0ee7a25c66d3ab248ce7af5e
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2023-12-15 19:20:40 +0100

    Initial Commit

---
+++ GoNilPointerDerefCrash/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GoNilPointerDerefCrash
+//
+//  Created by Hampus Sjöberg on 2023-12-15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
